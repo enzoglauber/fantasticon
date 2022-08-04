212b315c479096741b7d8a13b2fad37f297c8637
Enzo Glauber de Lima Almeida <enzo.almeida@bradesco.com.br>
2022-08-03 23:03:18 -0300

    templates and functional svg

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
+file:///C:/Users/EALM/git/fantasticon/dist/icons.html
+
 https://github.com/antoniodalessio/fristudio/blob/030769399d8f9d66f15bb5702bed927d153329d0/scripts/iconfont.mjs
--- scripts/iconfont.mjs
+++ scripts/iconfont.mjs
@@ -4,9 +4,9 @@ const baseDir = `./icons/`
 const destDir = `./dist/`
 
 async function init() {
-    
+
     const res = await generateFonts({
-        name: 'icons',
+        name: 'liquid-icons',
         fontTypes: [FontAssetType.EOT, FontAssetType.WOFF2, FontAssetType.WOFF],
         assetTypes: [
           OtherAssetType.CSS,
@@ -14,24 +14,31 @@ async function init() {
           OtherAssetType.JSON,
           OtherAssetType.TS
         ],
-        formatOptions: {},
+        formatOptions: {
+          json: {
+            indent: 2
+          }
+        },
         pathOptions: {},
         codepoints: {},
         //fontHeight: 300,
-        round: undefined, 
-        descent: undefined, 
+        round: undefined,
+        descent: undefined,
         normalize: undefined,
         selector: null,
         tag: 'i',
         prefix: 'icon',
         inputDir: `${baseDir}`,
         outputDir: `${destDir}`,
+        templates: {
+          css: `./templates/css.hbs`
+        },
         // fontsUrl: '../fonts/'
       })
 
-    // await fs.copy(`${baseDir}fonts/icons.css`, `${baseDir}scss/_icons.scss`) 
-    // await fs.copy(`${baseDir}fonts/icons.woff`, `${destDir}/icons.woff`) 
-    // await fs.copy(`${baseDir}fonts/icons.woff2`, `${destDir}/icons.woff2`) 
+    // await fs.copy(`${baseDir}fonts/icons.css`, `${baseDir}scss/_icons.scss`)
+    // await fs.copy(`${baseDir}fonts/icons.woff`, `${destDir}/icons.woff`)
+    // await fs.copy(`${baseDir}fonts/icons.woff2`, `${destDir}/icons.woff2`)
 }
 
 init()
